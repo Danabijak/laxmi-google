c9a1195892e85455e4573781a69bacc3af34a5f5
vodyrizam <vody.andrian14@gmail.com>
2018-10-17 03:23:27 +0700

    Done Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-$(function() {
-    $(".navbar.fixed-top").length && $(window).on("scroll touchmove", function(e) {
-        if (!($("#main-page").length <= 0)) {
-            $(this).scrollTop() > 200 ? $(".navbar.fixed-top").addClass("switch-color") : $(".navbar.fixed-top").removeClass("switch-color")
-        }
-    })
-})
+function addDot(nStr) {
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + '.' + '$2');
+	}
+	return x1 + x2;
+}
+
+$( function() {
+	$("#slider-Danabijak").slider({
+			range: "min",
+			value: 2000000,
+			min: 1000000,
+			max: 3000000,
+			step: 100000,
+			slide: function( event, ui ) {
+			  	$( "#amount" ).text( "Rp " + addDot(ui.value.toString()) );
+			}
+		});
+	$("#amount").text( "Rp " + $("#slider-Danabijak").slider("ui"));
+});
+
